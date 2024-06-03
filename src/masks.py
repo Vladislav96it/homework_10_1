6de84c96025e925da0cb7206e4df768348844640
vladislav usachev <fedya3864@gmail.com>
2024-06-03 20:23:42 +0300

    starting

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,16 @@
 def card_hide(card: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
-    return f"{card[:4]} {card[4:6] + '*' * 2} **** {card[-4:]}"
+    s = card.split()
+    number = [i for i in s if i.isdigit()]
+    number = ' '.join(number)
+    model = [i for i in s if i.isdigit() == False]
+    model = ' '.join(model)
+    mask = f"{number[:4]}{number[4:6]}2** {number[-4:]}"
+    return f"{model} {mask}"
 
 
-def masks_account_numbers(nums: str) ->str:
+def masks_account_numbers(score: str) -> str:
     """Функция, которая маскирует номер счета"""
-    return f"Счет **{nums[-4:]}"
+    numbers = score.split()[1]
+    mask = f'**{numbers[-4:]}'
+    return f"{score.split()[0]}"
