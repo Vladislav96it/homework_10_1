9908af8e370f007f6d584d43147af21e0f7d7e24
vladislav usachev <fedya3864@gmail.com>
2024-06-09 18:51:39 +0300

    написал вторую функцию в процессинг

--- src/processing.py
+++ src/processing.py
@@ -1,23 +1,23 @@
-def filter_state(operations: list[dict], state: str = 'EXECUTED') -> list[dict]:
+def filter_by_state(operations: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """функцию, которая принимает на вход список словарей
      и значение для ключа state(опциональный параметр со значением по умолчанию EXECUTED)
       и возвращает новый список,
      содержащий только те словари,
       у которых ключ state содержит переданное в функцию значение.
       """
-    return  [
+    return [
         operation
         for operation in operations
         if operation['state'] == state
     ]
 
 
-def sort_date(operations: list[dict], is_reverse: bool = True) -> list[dict]:
+def sort_by_date(operations: list[dict], is_reverse: bool = True) -> list[dict]:
     """функцию, которая принимает на вход список словарей и возвращает новый список,
      в котором исходные словари отсортированы по убыванию даты (ключ date.
  Функция принимает два аргумента, второй необязательный задает порядок сортировки (убывание, возрастание).
     """
     return [
         operation
-        for operation in sorted(operations, key=lambda x:['date'], reverse=is_reverse)
+        for operation in sorted(operations, key=lambda x: ['date'], reverse=is_reverse)
     ]
