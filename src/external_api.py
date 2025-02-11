e284fcaad8407807695c575ab04165156171241e
vladislav usachev <fedya3864@gmail.com>
2025-02-11 19:36:08 +0300

    13.1 первая сдача

--- src/external_api.py
+++ src/external_api.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 URL = "https://api.apilayer.com/exchangerates_data/latest"
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
+
+
 def get_exchange_rate(base_currency: str, target_currency: str = "RUB") -> Any:
     """ Получаем курс валют к рублю """
     headers = {"apikey": API_KEY}
@@ -15,6 +17,8 @@ def get_exchange_rate(base_currency: str, target_currency: str = "RUB") -> Any:
         return data["rates"][target_currency]
     else:
         raise Exception(f"Ошибка запроса {response.status_code}")
+
+
 def convert_transaction_to_rubles(transaction: Dict) -> Any:
     """ Конвертируем в рубли """
     amount = transaction["amount"]
@@ -24,11 +28,14 @@ def convert_transaction_to_rubles(transaction: Dict) -> Any:
     else:
         exchange_rate = get_exchange_rate(currency)
         return round(exchange_rate * float(amount), 2)
+
+
 # Пример использования
 if __name__ == "__main__":
     example_transaction = {
         'amount': 100,
         'currency': 'USD'
+
     }
     rubles_amount = convert_transaction_to_rubles(example_transaction)
     print(f"Transaction amount in RUB: {rubles_amount}")
